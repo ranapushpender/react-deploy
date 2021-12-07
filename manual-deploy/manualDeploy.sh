31ebca4facd947c45d2e926c30c051376681c83e
Pushpender <34644707+ranapushpender@users.noreply.github.com>
2021-12-07 13:56:58 +0530

    Update manualDeploy.sh

--- manual-deploy/manualDeploy.sh
+++ manual-deploy/manualDeploy.sh
@@ -1,8 +1,9 @@
-source config.sh
+#!/bin/bash
+source src/scripts/config.sh
 branchname=`git rev-parse --abbrev-ref HEAD`
 mv build $branchname
 echo $USERNAME $SERVER
 ssh -o StrictHostKeyChecking=no $USERNAME@$SERVER rm -rf /$DEPLOY_PATH/react-deploy/projects/$branchname
-scp -o StrictHostKeyChecking=no -r ./$branchname $USERNAME@$SERVER:/$DEPLOY_PATH/react-deploy/projects/
+scp -o StrictHostKeyChecking=no -r $branchname $USERNAME@$SERVER:/$DEPLOY_PATH/react-deploy/projects/
 ssh -o StrictHostKeyChecking=no $USERNAME@$SERVER 'docker exec -i nginx sh /scripts/generate-config.sh'
 mv $branchname build
