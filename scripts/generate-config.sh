8fd5bd9428a6d1f317cda4a10d7cc600282ba916
Pushpender Rana <pushpender661@gmail.com>
2021-12-08 20:00:30 +0530

    Update nginx path

--- scripts/generate-config.sh
+++ scripts/generate-config.sh
@@ -1,4 +1,4 @@
 ls /projects | while read line ; do sed 's/\$\$\$\$\$/'$line'/g' /scripts/template.conf > /etc/nginx/conf.d/$line.conf ; done
-nginx -s reload
+/usr/local/openresty/nginx/sbin -s reload
 
 
