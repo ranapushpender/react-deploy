5a9c5649096fca8cbe81bc036db275f1a1116021
Pushpender Rana <pushpender661@gmail.com>
2021-12-08 20:03:28 +0530

    Endpoint name fix

--- scripts/generate-config.sh
+++ scripts/generate-config.sh
@@ -1,4 +1,4 @@
 ls /projects | while read line ; do sed 's/\$\$\$\$\$/'$line'/g' /scripts/template.conf > /etc/nginx/conf.d/$line.conf ; done
-/usr/local/openresty/nginx/sbin -s reload
+/usr/local/openresty/nginx/sbin/nginx -s reload
 
 
